33f81dfb6878a3911a83eaa40b9b02196da8296f
Force Charlie <ipvb@qq.com>
2016-01-13 16:56:31 +0800

    Add build.ps1 Function

--- build.ps1
+++ build.ps1
@@ -2,3 +2,30 @@
 #>
 
 $PrefixDir=[System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
+$SourcesDir="$PrefixDir/src"
+
+Function Build-Sources{
+    param(
+        [String]$SrcDir,
+        [String]$Include
+    )
+    Push-Location $PWD
+    if((Test-Path "$PrefixDir/obj")){
+        mkdir "$PrefixDir/obj"
+    }
+    Set-Location "$PrefixDir/obj"
+    $filelist=Get-ChildItem "$SrcDir"  -recurse *.cc | %{$_.FullName}
+    foreach($file in filelist){
+        #Build File
+        IEX "cl -c $file -O2 -I$SrcDir"
+    }
+    Pop-Location
+}
+
+Function Link-Domake{
+    param(
+        [String]$ObjectDir
+    )
+    Push-Location $PWD
+    Pop-Location
+}
