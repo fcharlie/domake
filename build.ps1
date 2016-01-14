3411089777da9e3fa1f8573dfb7d83f659692c92
Force Charlie <charlieio@outlook.com>
2016-01-14 22:40:20 +0800

    Add Cmdlet

--- build.ps1
+++ build.ps1
@@ -23,7 +23,7 @@ Function Start-CompileDomake{
     $filelist=Get-ChildItem "$SrcDir"  -Recurse *.cc | Foreach {$_.FullName}
     foreach($file in $filelist){
         #Build File
-        &cl -c $file -O2 -I$SrcDir
+        &cl -c $file -O2 -TP  -W4 -EHsc -Zc:forScope -Zc:wchar_t -MT -I$SrcDir
     }
     Pop-Location
 }
@@ -35,9 +35,27 @@ Function Start-LinkDomake{
         [String]$ObjectDir
     )
     Push-Location $PWD
-    &link 
+    &link "$ObjectDir\*.obj" KERNEL32.lib   ADVAPI32.lib Shell32.lib USER32.lib GDI32.lib comctl32.lib Shlwapi.lib Secur32.lib
     Pop-Location
 }
 
+Function Restore-Environment{
+    param(
+        [Parameter(Position=0,Mandatory=$True,HelpMessage="Enter Root Folder")]
+        [String]$Root=$PrefixDir
+    )
+    #Check Root
+}
+
+Function Clear-Domake{
+    #
+}
+
+param(
+    [ValidateSet("build","rebuild","clear")]
+    [String]$action="build"
+)
+
+Restore-Environment
 Start-CompileDomake -SrcDir $SourcesDir -Include "$PrefixDir/include"
 Start-LinkDomake -ObjectDir $ObjectDir
