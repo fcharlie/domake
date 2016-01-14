431a1de9f20a7f20402f3855d06f5a5e4e8dabf9
Force Charlie <charlieio@outlook.com>
2016-01-14 21:44:03 +0800

    build.ps1 safe

--- build.ps1
+++ build.ps1
@@ -3,29 +3,41 @@
 
 $PrefixDir=[System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
 $SourcesDir="$PrefixDir/src"
+$ObjectDir="$PrefixDir/obj"
 
-Function Compile-Domake{
+<#
+# Compile-Domake Function
+#>
+Function Start-CompileDomake{
     param(
+        [Parameter(Position=0,Mandatory=$True,HelpMessage="Enter Sources Directory")]
+        [ValidateNotNullorEmpty()]
         [String]$SrcDir,
         [String]$Include
     )
     Push-Location $PWD
-    if((Test-Path "$PrefixDir/obj")){
-        mkdir "$PrefixDir/obj"
+    if(!(Test-Path "$PrefixDir/obj")){
+        mkdir -Force "$PrefixDir/obj"
     }
     Set-Location "$PrefixDir/obj"
-    $filelist=Get-ChildItem "$SrcDir"  -recurse *.cc | %{$_.FullName}
-    foreach($file in filelist){
+    $filelist=Get-ChildItem "$SrcDir"  -Recurse *.cc | Foreach {$_.FullName}
+    foreach($file in $filelist){
         #Build File
-        IEX "cl -c $file -O2 -I$SrcDir"
+        &cl -c $file -O2 -I$SrcDir
     }
     Pop-Location
 }
 
-Function Link-Domake{
+Function Start-LinkDomake{
     param(
+        [Parameter(Position=0,Mandatory=$True,HelpMessage="Enter Objects Directory")]
+        [ValidateNotNullorEmpty()]
         [String]$ObjectDir
     )
     Push-Location $PWD
+    &link 
     Pop-Location
 }
+
+Start-CompileDomake -SrcDir $SourcesDir -Include "$PrefixDir/include"
+Start-LinkDomake -ObjectDir $ObjectDir
