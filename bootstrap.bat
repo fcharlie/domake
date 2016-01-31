8d63b6905556ab644ff4cc2ded1ed1298f7c76e5
Force Charlie <charlieio@outlook.com>
2016-01-31 21:50:04 +0800

    update bootstrap.bat ,Fix set env failed

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,6 +1,10 @@
 @echo off
 REM Call MSVC Environment
 
+if "%ToolChian%"=="" goto EnvironmentSetting
+goto DomakeBuildTask
+
+:EnvironmentSetting
 if "%CPUArch%"=="" for /F "tokens=1,2*" %%i in ('reg query "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" /v "PROCESSOR_ARCHITECTURE"') do (
     if "%%i" =="PROCESSOR_ARCHITECTURE" (
         set CPUArch=%%k
@@ -24,12 +28,14 @@ goto VS140USE32BIT
 :ARM
 if not exist "%VS140COMNTOOLS%..\..\VC\bin\x86_arm" goto VAILDVSNOTFOUD
 call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"  x86_arm
-goto LauncherBuildTask
+set ToolChian=Visual Studio 2015 ARM
+goto DomakeBuildTask
 
 :ARM64
 if not exist "%VS140COMNTOOLS%..\..\VC\bin\x86_arm64" goto VAILDVSNOTFOUD
 call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"  x86_arm64
-goto LauncherBuildTask
+set ToolChian=Visual Studio 2015 ARM64
+goto DomakeBuildTask
 
 goto :EOF
 
@@ -40,16 +46,19 @@ goto :EOF
 :VS140USE64BIT
 if not exist "%VS140COMNTOOLS%..\..\VC\bin\x86_amd64" goto VS120USE64BIT
 call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"  x86_amd64
+set ToolChian=Visual Studio 2015 x64
 goto DomakeBuildTask
 
 :VS120USE64BIT
 IF not exist "%VS120COMNTOOLS%..\..\VC\bin\x86_amd64"  goto VS110USE64BIT
 call "%VS120COMNTOOLS%..\..\VC\vcvarsall.bat"  x86_amd64
+set ToolChian=Visual Studio 2013 x64
 goto DomakeBuildTask
 
 :VS110USE64BIT
 if not exist "%VS110COMNTOOLS%..\..\VC\bin\x86_amd64"  goto VAILDVSNOTFOUD
 call "%VS110COMNTOOLS%..\..\VC\vcvarsall.bat"  x86_amd64
+set ToolChian=Visual Studio 2012 x64
 goto DomakeBuildTask
 
 
@@ -57,16 +66,19 @@ goto DomakeBuildTask
 :VS140USE32BIT
 if not exist %VS140COMNTOOLS%  goto VS120USE32BIT
 call "%VS140COMNTOOLS%\VsDevCmd.bat"
+set ToolChian=Visual Studio 2015
 goto DomakeBuildTask
 
 :VS120USE32BIT
 IF not exist "%VS120COMNTOOLS%"  goto VS110USE32BIT
 call "%VS120COMNTOOLS%.\VsDevCmd.bat"
+set ToolChian=Visual Studio 2013
 goto DomakeBuildTask
 
 :VS110USE32BIT
 if not exist %VS110COMNTOOLS%  goto VAILDVSNOTFOUD
 call "%VS110COMNTOOLS%\VsDevCmd.bat"
+set ToolChian=Visual Studio 2012
 goto DomakeBuildTask
 
 :VAILDVSNOTFOUD
--- src/runtime.h
+++ src/runtime.h
@@ -7,13 +7,27 @@
 #include <string>
 
 #ifdef _WIN32
+
 typedef wchar_t Char;
 typedef std::wstring String;
 #define DEFAULT_USERAGENT L"domake/1.0"
+
+#ifndef _T
+#define _T(x) __T(x)
+#define __T(x) L##x
+#endif
+
 #else
+
+#ifndef _T
+#define _T(x) __T(x)
+#define __T(x) L##x
+#endif
+
 typedef char Char;
 typedef std::string String;
 #define DEFAULT_USERAGENT "domake/1.0"
+
 #endif
 
 #endif
